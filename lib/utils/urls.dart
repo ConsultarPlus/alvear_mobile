02eb6a609d11c8cfcf28901f097353e269183eb6
software@consultar.org <Saavedra_1175>
2022-06-07 09:25:38 -0300

    Login

--- lib/utils/urls.dart
+++ lib/utils/urls.dart
@@ -1,5 +1,6 @@
 urlDominio(){
-  return 'http://190.193.200.120:1088/';
+  return 'http://calvear-medidores.dynalias.net/';
+  // return 'http://190.193.200.120:1088/';
   //return 'http://192.168.1.32:1088/';
   //return 'http://10.0.2.2:8000/';
 }
